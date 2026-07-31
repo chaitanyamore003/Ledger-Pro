a590902cac9b4c87e9caea0a9455ce12eb9468ca
chaitanyamore003 <chaitanyamore003@gmail.com>
2026-07-31 23:11:33 +0530

    deploy

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,8 +4,9 @@ const app = require("./src/app");
 const config = require("./src/config/config");
 const connectToDB = require("./src/config/db");
 
-connectToDB();
+require("./src/services/email.service");
 
+connectToDB();
 
 app.listen(config.PORT, () => {
   console.log(`Server Running on PORT - http://localhost:${config.PORT}`);
--- FrontEnd/src/services/refreshClient.js
+++ FrontEnd/src/services/refreshClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const refreshClient = axios.create({
-  baseURL: "http://localhost:3080/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
